f0a0f93296b2a992988d1a83f43597228cd2ee29
donut0310 <donut0310@naver.com>
2020-03-20 01:27:06 +0900

    express folder add

--- project/public/js/custom/Daycarejs.js
+++ project/public/js/custom/Daycarejs.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:project/public/js/custom/Daycarejs.js
 const ul = document.querySelector('.postlist');
 
 const pmpc_btn = document.querySelector('#iPost_more');
@@ -28,35 +27,4 @@ function initShow(item = []){
 }
 
 
-=======
-const ul = document.querySelector('.postlist');
-
-const pmpc_btn = document.querySelector('#iPost_more');
-const meal_btn = document.querySelector('#iPost_more');
-
-function init(){
-    axios.post('/Daycare/init').then((res)=>{
-        if(res.status === 200){
-            if(res.data["result"] == "success"){ 
-                    initShow(res.data["data"]);
-            }
-        }
-    });
-}
-
-function initShow(item = []){
-    item.forEach(function(data){
-        let url = document.createElement('a');
-        url.href = '../../html/pmpc/cus_pmpc_board.html?' + data.content_order;
-        url.innerText = data.title;
-        
-        let li = document.createElement('li');
-        li.className += 'list';
-        li.appendChild(url);
-        ul.appendChild(li);
-    });
-}
-
-
->>>>>>> a89133af6e8e3eb9a12d00aaf4deb029fd402ae9:js/custom/Daycarejs.js
 init();
