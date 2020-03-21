af463695f516435c6ec0e2bdaecb95d7f2cc6080
donut0310 <donut0310@naver.com>
2020-03-22 04:08:53 +0900

    daycare img load

--- project/routes/Daycare.js
+++ project/routes/Daycare.js
@@ -8,11 +8,30 @@ var dbconfig = require('./config/dbconfig.js');
 var pool = mysql.createPool(dbconfig);
 
 
-router.post('/init', function(req, res, next){
+router.post('/initPostlist', function(req, res, next){
     pool.getConnection(function(err,conn){
         if(err) throw err;
         else{
             let sql = "select * from postlist order by content_order desc limit 0,10";
+            conn.query(sql, function(err,rows){
+                if(err)throw err;
+                else{
+                    conn.release();
+                    let output = {};
+                    output['result'] = "success";
+                    output['data'] = rows;
+                    res.send(output);
+            }
+        });
+     }
+    });
+});
+
+router.post('/initPhoto', function(req, res, next){
+    pool.getConnection(function(err,conn){
+        if(err) throw err;
+        else{
+            let sql = "select * from img order by file_order desc limit 0,10";
             conn.query(sql, function(err,rows){
                 if(err)throw err;
                 else{
