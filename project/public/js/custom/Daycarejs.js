839aff53784c20aa95b7a972ac484cb12623acc7
hyeonsik <hs0208_@naver.com>
2020-03-23 00:51:20 +0900

    mod_dacarejs

--- project/public/js/custom/Daycarejs.js
+++ project/public/js/custom/Daycarejs.js
@@ -19,6 +19,8 @@ function init() {
         }
     });
     openMsgBox();
+
+    setInterval(slide, 2000);
 }
 
 function showPostList(item = []) {
@@ -34,14 +36,14 @@ function showPostList(item = []) {
     });
 }
 
-function showPhoto(item =[]){
+function showPhoto(item = []) {
     let cnt = 1;
-    item.forEach(function(data){
-        resizeImg(data,cnt);
-        cnt+=2;
+    item.forEach(function (data) {
+        resizeImg(data, cnt);
+        cnt += 2;
     });
 }
-function resizeImg(data,cnt) {
+function resizeImg(data, cnt) {
     let photozone = document.querySelector('.photozone');
     let photos = photozone.childNodes;
 
@@ -49,38 +51,65 @@ function resizeImg(data,cnt) {
     img.src = data.path + data.file_name;
     img.width = 195;
     img.height = 120;
-    
+
     photos[cnt].appendChild(img);
 }
-    
- //메인 페이지 팝업창
+
+//메인 페이지 팝업창
 function getCookie(name) {
-   var Found = false
-   var start, end
-   var i = 0
-   while (i <= document.cookie.length) {
-       start = i
-       end = start + name.length
-       if (document.cookie.substring(start, end) == name) {
-           Found = true
-           break
-       }
-       i++
-   }
-   if (Found == true) {
-       start = end + 1
-       end = document.cookie.indexOf(";", start)
-       if (end < start)
-           end = document.cookie.length
-       return document.cookie.substring(start, end)
-   }
-   return ""
+    var Found = false
+    var start, end
+    var i = 0
+    while (i <= document.cookie.length) {
+        start = i
+        end = start + name.length
+        if (document.cookie.substring(start, end) == name) {
+            Found = true
+            break
+        }
+        i++
+    }
+    if (Found == true) {
+        start = end + 1
+        end = document.cookie.indexOf(";", start)
+        if (end < start)
+            end = document.cookie.length
+        return document.cookie.substring(start, end)
+    }
+    return ""
 }
 
 function openMsgBox() {
     var eventCookie = getCookie("memo");
     if (eventCookie != "no")
         window.open('/popup', 'popup file', 'width=345px,height=722px,top=50,left=150');
+}
+
+//메인 페이지 배너
+var firstslide = document.querySelector(".carousel-box:first-child");
+var active = "active";
+function slide() {
+    var currentslide = document.querySelector(".active");
+
+    if (currentslide) {
+
+        currentslide.classList.remove(active);
+
+        var nextslide = currentslide.nextElementSibling;
+
+        if (nextslide) {
+            nextslide.classList.add(active);
+        }
+
+        else {
+            firstslide.classList.add(active);
+        }
+    }
+    else {
+        firstslide.classList.add(active);
+    }
+
+
 }
 
 init();
