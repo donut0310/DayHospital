7fb82bbc80df3d48105f308bfd5b9c89414f09d8
donut0310 <donut0310@naver.com>
2020-04-10 22:29:43 +0900

    edit facility

--- project/public/js/custom/facilityjs.js
+++ project/public/js/custom/facilityjs.js
@@ -1,96 +1,31 @@
-// modal
-const imgModal = document.querySelector('#modal-in-photo');
-const err_modal = document.querySelector('#err_modal');
-const err_close = document.querySelector("#err_close");
-err_close.addEventListener('mouseover',getCursor);
-
-//btns
-const imgCloseBtn = document.querySelector('.imgCloseBtn');
-imgCloseBtn.addEventListener('mouseover',getCursor);
-
-const prevBtn = document.querySelector('.prevBtn');
-prevBtn.addEventListener('click',goToPrev);
-prevBtn.addEventListener('mouseover',getCursor);
-
-const nextBtn = document.querySelector('.nextBtn');
-nextBtn.addEventListener('click',goToNext);
-nextBtn.addEventListener('mouseover',getCursor);
-
-//parmas
-let currentImgNum; //모달에 로드된 이미지 id값
+const photos = document.querySelectorAll('.fa img');
+const bigPhoto = document.querySelector('#fa_photo_big img');
+const photoDiv = document.querySelectorAll('.fa');
+//params
+let currentValue = 0;
 
 function init(){
-    let images = document.querySelectorAll('.facility_photo img');
     
-    images.forEach(function(data){
-        data.addEventListener('mouseover',getCursor);
-        data.addEventListener('click',getImgModal);
-    });
+    for(i=0;i<6;i++){
+        if(i==0){
+            bigPhoto.src = photos[i].src;
+        }
+        photos[i].addEventListener('mouseover',getCursor);        
+        photos[i].addEventListener('click',loadImage); 
+        photos[i].setAttribute('imgid',i);
+    }   
 }
 
-function getImgModal(){
-    //console.log(this);
-    let modalImg = document.querySelector('#modalImg');
-    modalImg.src = this.getAttribute('src');
-    currentImgNum = Number(this.getAttribute('imgid'));
-    modalImg.setAttribute('imgid',currentImgNum);
-
-    imgModal.style.display = 'block';
+function loadImage(){
+    console.log(photos[currentValue]);
+    photoDiv[currentValue].classList.remove('fa_current');
+    
+    currentValue = this.getAttribute('imgid');
+    photoDiv[currentValue].classList.add('fa_current');
+    
+    bigPhoto.src = this.src;
 }
-
 function getCursor(){
-    this.style.cursor = 'pointer';
+    this.style.cursor = "pointer";
 }
-
-// 이전 사진으로
-function goToPrev(){
-    let modalImg = document.querySelector('#modalImg');
-    currentImgNum = Number(modalImg.getAttribute('imgid'));
-    if(currentImgNum == 1){
-        err_modal.style.display = 'block';
-    }
-    else{
-        let prevImg = document.querySelector('#fa_photo' + (currentImgNum - 1) + ' img');
-        modalImg.src = prevImg.getAttribute('src');
-        modalImg.setAttribute('imgid',currentImgNum - 1);
-
-    }
-}
-
-// 다음사진으로
-function goToNext(){
-    let modalImg = document.querySelector('#modalImg');
-    currentImgNum = Number(modalImg.getAttribute('imgid'));
-    
-    if(currentImgNum == 6){
-        err_modal.style.display = 'block';
-    }
-    else{
-        let nextImg = document.querySelector('#fa_photo' + (currentImgNum + 1) + ' img');
-        modalImg.src = nextImg.getAttribute('src'); 
-        modalImg.setAttribute('imgid',currentImgNum + 1);
-    }
-}
-
-// modal 종료 함수
-imgCloseBtn.onclick = function(){
-    imgModal.style.display = 'none';
-}
-
-err_close.onclick = function(){
-    err_modal.style.display = 'none';
-}
-
-window.onclick = function(event){
-    if(event.target == imgModal){
-        imgModal.style.display = 'none';
-    }
-}
-
-window.onkeydown = function(){
-    if(event.keyCode == 27){
-        imgModal.style.display = 'none';
-    }
-}
-
 init();
