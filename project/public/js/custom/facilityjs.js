ed30fd37e97859cc1f46ac09c079517fdedfce76
JerryJ <shin961112@gmail.com>
2020-05-18 22:49:18 +0900

    facility mobile slide show fix

--- project/public/js/custom/facilityjs.js
+++ project/public/js/custom/facilityjs.js
@@ -1,13 +1,17 @@
 //Facility Slide Show PC
 let slideIndex = 1;
+let mslideIndex = 1;
 showSlides(slideIndex); //first slide show on
+mobileShowSlides(mslideIndex);
 
 function plusSlides(n){ // click prev, next
     showSlides(slideIndex += n);
+    mobileShowSlides(mslideIndex+=n);
 }
 
 function currentSlide(n) { //click dot
     showSlides(slideIndex = n);
+    mobileShowSlides(mslideIndex = n);
 }
 
 function showSlides(n){
@@ -28,5 +32,25 @@ function showSlides(n){
 
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
+}
+
+function mobileShowSlides(n){
+    let j=0;
+    let mslides = document.getElementsByClassName("mobile-facilitySlide");
+    let mdots = document.getElementsByClassName("mdot");
+
+    if(n>mslides.length) {mslideIndex=1};
+    if(n<1){mslideIndex = mslides.length};
+
+    for(j=0;j<mslides.length;j++){ //init
+        mslides[j].style.display = 'none';
+    }
+
+    for(j=0;j<mdots.length;j++){ //init
+        mdots[j].className = mdots[j].className.replace(" active","");
+    }
+
+    mslides[mslideIndex-1].style.display = "block";
+    mdots[mslideIndex-1].className += " active";
 }
 
