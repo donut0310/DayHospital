5ff89a0bfffa826c22fc317af5c538509e4ce583
JerryJ <shin961112@gmail.com>
2020-06-03 01:46:37 +0900

    delete naver map

--- project/public/js/custom/map.js
+++ project/public/js/custom/map.js
@@ -1,82 +1,44 @@
-// var mapContainer = document.querySelector('.map');
-// //var mobile_mapContainer = document.querySelector('.mobile_main_map');
+var mapContainer = document.querySelector('.map');
+//var mobile_mapContainer = document.querySelector('.mobile_main_map');
 
-// var options = {
-//     center: new kakao.maps.LatLng(37.657607, 127.045947),
-//     level: 3
-// };
+var options = {
+    center: new kakao.maps.LatLng(37.657607, 127.045947),
+    level: 3
+};
 
-// var map = new kakao.maps.Map(mapContainer, options);
-// //var mobile_map = new kakao.maps.Map(mobile_mapContainer,options);
-// // 마커가 표시될 위치입니다 
-// var markerPosition  = new kakao.maps.LatLng(37.657607, 127.045947); 
+var map = new kakao.maps.Map(mapContainer, options);
+//var mobile_map = new kakao.maps.Map(mobile_mapContainer,options);
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.657607, 127.045947); 
 
-// // 마커를 생성합니다
-// var marker = new kakao.maps.Marker({
-//     position: markerPosition
-// });
-
-// // 마커가 지도 위에 표시되도록 설정합니다
-// marker.setMap(map);
-// //marker.setMap(mobile_map);
-
-// // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-// // marker.setMap(null); 
-// // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
-// var iwContent = '<div style="padding:8px;">성모사랑센터</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-
-// // 인포윈도우를 생성합니다
-// var infowindow = new kakao.maps.InfoWindow({
-//     content : iwContent
-// });
-
-// // 마커에 마우스오버 이벤트를 등록합니다
-// kakao.maps.event.addListener(marker, 'mouseover', function() {
-//   // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-//     infowindow.open(map, marker);
-// });
-
-// // 마커에 마우스아웃 이벤트를 등록합니다
-// kakao.maps.event.addListener(marker, 'mouseout', function() {
-//     // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-//     infowindow.close();
-// });
-
-
-//지도를 삽입할 HTML 요소 또는 HTML 요소의 id를 지정합니다.
-var HOME_PATH = window.HOME_PATH || '.';
-
-var daycare = new naver.maps.LatLng(37.657607, 127.045947),
-    map = new naver.maps.Map('map', {
-        center: daycare.destinationPoint(0, 500),
-        zoom: 15
-    }),
-    marker = new naver.maps.Marker({
-        map: map,
-        position: daycare
-    });
-
-var contentString = [
-        '<div class="iw_inner">',
-        '   <h3>서울특별시청</h3>',
-        '   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />',
-        '       <img src="'+ HOME_PATH +'/img/example/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
-        '       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />',
-        '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
-        '   </p>',
-        '</div>'
-    ].join('');
-
-var infowindow = new naver.maps.InfoWindow({
-    content: contentString
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
 });
 
-naver.maps.Event.addListener(marker, "click", function(e) {
-    if (infowindow.getMap()) {
-        infowindow.close();
-    } else {
-        infowindow.open(map, marker);
-    }
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+//marker.setMap(mobile_map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null); 
+// 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+var iwContent = '<div style="padding:8px;">성모사랑센터</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    content : iwContent
 });
 
-infowindow.open(map, marker);
+// 마커에 마우스오버 이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'mouseover', function() {
+  // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+    infowindow.open(map, marker);
+});
+
+// 마커에 마우스아웃 이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'mouseout', function() {
+    // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+    infowindow.close();
+});
+
