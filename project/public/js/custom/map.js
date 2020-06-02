b2187d6605bd6c42fbe2a5e93769b314a5a8795c
JerryJ <shin961112@gmail.com>
2020-06-03 01:43:40 +0900

    mod map

--- project/public/js/custom/map.js
+++ project/public/js/custom/map.js
@@ -44,11 +44,39 @@
 
 
 //지도를 삽입할 HTML 요소 또는 HTML 요소의 id를 지정합니다.
-var mapDiv = document.querySelector('.map'); // 'map'으로 선언해도 동일
+var HOME_PATH = window.HOME_PATH || '.';
 
-var mapOptions = {
-    center : new naver.maps.LatLng(37.657607, 127.045947),
-    zoom : 10
-};
+var daycare = new naver.maps.LatLng(37.657607, 127.045947),
+    map = new naver.maps.Map('map', {
+        center: daycare.destinationPoint(0, 500),
+        zoom: 15
+    }),
+    marker = new naver.maps.Marker({
+        map: map,
+        position: daycare
+    });
 
-var map = new naver.maps.Map('map',mapOptions);
+var contentString = [
+        '<div class="iw_inner">',
+        '   <h3>서울특별시청</h3>',
+        '   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />',
+        '       <img src="'+ HOME_PATH +'/img/example/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
+        '       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />',
+        '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
+        '   </p>',
+        '</div>'
+    ].join('');
+
+var infowindow = new naver.maps.InfoWindow({
+    content: contentString
+});
+
+naver.maps.Event.addListener(marker, "click", function(e) {
+    if (infowindow.getMap()) {
+        infowindow.close();
+    } else {
+        infowindow.open(map, marker);
+    }
+});
+
+infowindow.open(map, marker);
